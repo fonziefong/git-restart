989b55e7934b9bec063acd4275e831e678e23b44
Fonzie Fong <fonziefong@gmail.com>
2021-11-10 11:59:25 +0800

    Create .NET APP
    Add several files and modify README.md

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_restart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Git Restart
 
 I'm coming back!
+
+## Task 1
+
+I'm trying to commit.
